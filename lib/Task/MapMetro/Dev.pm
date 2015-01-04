bac77acab59c12bf26091c515b502469b430c9da
Erik Carlsson <info@code301.com>
2015-01-04 18:45:23 +0100

    "Init."

---
+++ lib/Task/MapMetro/Dev.pm
@@ -0,0 +1,28 @@
+package Task::MapMetro::Dev;
+
+use strict;
+use warnings;
+use 5.10.1;
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Task::MapMetro::Dev - Short intro
+
+=head1 SYNOPSIS
+
+  use Task::MapMetro::Dev;
+
+=head1 DESCRIPTION
+
+Task::MapMetro::Dev is ...
+
+=head1 SEE ALSO
+
+=cut
